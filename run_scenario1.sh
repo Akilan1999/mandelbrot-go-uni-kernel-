1403c64e72d964da93b017e1cbce241c46a80462
Akilan <me@akilan.io>
2022-08-10 11:05:55 +0100

    added run scripts

---
+++ run_scenario1.sh
@@ -0,0 +1,70 @@
+# Run scenarios
+
+./test -cores 1 -height 1000 -iteration 3000 > result1-1core.txt
+echo "1"
+./test -cores 1 -height 1000 -iteration 3000 > result2-1core.txt
+echo "2"
+./test -cores 1 -height 1000 -iteration 3000 > result3-1core.txt
+echo "3"
+./test -cores 1 -height 1000 -iteration 3000 > result4-1core.txt
+echo "4"
+./test -cores 1 -height 1000 -iteration 3000 > result5-1core.txt
+echo "5"
+./test -cores 1 -height 1000 -iteration 3000 > result6-1core.txt
+echo "6"
+./test -cores 1 -height 1000 -iteration 3000 > result7-1core.txt
+echo "7"
+./test -cores 1 -height 1000 -iteration 3000 > result8-1core.txt
+echo "8"
+
+./test -cores 4 -height 1000 -iteration 3000 > result1-4core.txt
+echo "1"
+./test -cores 4 -height 1000 -iteration 3000 > result2-4core.txt
+echo "2"
+./test -cores 4 -height 1000 -iteration 3000 > result3-4core.txt
+echo "3"
+./test -cores 4 -height 1000 -iteration 3000 > result4-4core.txt
+echo "4"
+./test -cores 4 -height 1000 -iteration 3000 > result5-4core.txt
+echo "5"
+./test -cores 4 -height 1000 -iteration 3000 > result6-4core.txt
+echo "6"
+./test -cores 4 -height 1000 -iteration 3000 > result7-4core.txt
+echo "7"
+./test -cores 4 -height 1000 -iteration 3000 > result8-4core.txt
+echo "8"
+
+./test -cores 6 -height 1000 -iteration 3000 > result1-6core.txt
+echo "1"
+./test -cores 6 -height 1000 -iteration 3000 > result2-6core.txt
+echo "2"
+./test -cores 6 -height 1000 -iteration 3000 > result3-6core.txt
+echo "3"
+./test -cores 6 -height 1000 -iteration 3000 > result4-6core.txt
+echo "4"
+./test -cores 6 -height 1000 -iteration 3000 > result5-6core.txt
+echo "5"
+./test -cores 6 -height 1000 -iteration 3000 > result6-6core.txt
+echo "6"
+./test -cores 6 -height 1000 -iteration 3000 > result7-6core.txt
+echo "7"
+./test -cores 6 -height 1000 -iteration 3000 > result8-6core.txt
+echo "8"
+
+./test -cores 8 -height 1000 -iteration 3000 > result1-8core.txt
+echo "1"
+./test -cores 8 -height 1000 -iteration 3000 > result2-8core.txt
+echo "2"
+./test -cores 8 -height 1000 -iteration 3000 > result3-8core.txt
+echo "3"
+./test -cores 8 -height 1000 -iteration 3000 > result4-8core.txt
+echo "4"
+./test -cores 8 -height 1000 -iteration 3000 > result5-8core.txt
+echo "5"
+./test -cores 8 -height 1000 -iteration 3000 > result6-8core.txt
+echo "6"
+./test -cores 8 -height 1000 -iteration 3000 > result7-8core.txt
+echo "7"
+./test -cores 8 -height 1000 -iteration 3000 > result8-8core.txt
+echo "8"
+
---
+++ run_scenario2.sh
@@ -0,0 +1,69 @@
+# Run scenarios
+
+./test -cores 1 -height 2000 -iteration 6000 > result-scenario2-1core.txt
+echo "1"
+./test -cores 1 -height 2000 -iteration 6000 > result-scenario2-2-1core.txt
+echo "2"
+./test -cores 1 -height 2000 -iteration 6000 > result-scenario2-3-1core.txt
+echo "3"
+./test -cores 1 -height 2000 -iteration 6000 > result-scenario2-4-1core.txt
+echo "4"
+./test -cores 1 -height 2000 -iteration 6000 > result-scenario2-5-1core.txt
+echo "5"
+./test -cores 1 -height 2000 -iteration 6000 > result-scenario2-6-1core.txt
+echo "6"
+./test -cores 1 -height 2000 -iteration 6000 > result-scenario2-7-1core.txt
+echo "7"
+./test -cores 1 -height 2000 -iteration 6000 > result-scenario2-8-1core.txt
+echo "8"
+
+./test -cores 4 -height 2000 -iteration 6000 > result-scenario2-1-4core.txt
+echo "1"
+./test -cores 4 -height 2000 -iteration 6000 > result-scenario2-2-4core.txt
+echo "2"
+./test -cores 4 -height 2000 -iteration 6000 > result-scenario2-3-4core.txt
+echo "3"
+./test -cores 4 -height 2000 -iteration 6000 > result-scenario2-4-4core.txt
+echo "4"
+./test -cores 4 -height 2000 -iteration 6000 > result-scenario2-5-4core.txt
+echo "5"
+./test -cores 4 -height 2000 -iteration 6000 > result-scenario2-6-4core.txt
+echo "6"
+./test -cores 4 -height 2000 -iteration 6000 > result-scenario2-7-4core.txt
+echo "7"
+./test -cores 4 -height 2000 -iteration 6000 > result-scenario2-8-4core.txt
+echo "8"
+
+./test -cores 6 -height 2000 -iteration 6000 > result-scenario2-1-6core.txt
+echo "1"
+./test -cores 6 -height 2000 -iteration 6000 > result-scenario2-2-6core.txt
+echo "2"
+./test -cores 6 -height 2000 -iteration 6000 > result-scenario2-3-6core.txt
+echo "3"
+./test -cores 6 -height 2000 -iteration 6000 > result-scenario2-4-6core.txt
+echo "4"
+./test -cores 6 -height 2000 -iteration 6000 > result-scenario2-5-6core.txt
+echo "5"
+./test -cores 6 -height 2000 -iteration 6000 > result-scenario2-6-6core.txt
+echo "6"
+./test -cores 6 -height 2000 -iteration 6000 > result-scenario2-7-6core.txt
+echo "7"
+./test -cores 6 -height 2000 -iteration 6000 > result-scenario2-8-6core.txt
+echo "8"
+
+./test -cores 8 -height 2000 -iteration 6000 > result-scenario2-1-8core.txt
+echo "1"
+./test -cores 8 -height 2000 -iteration 6000 > result-scenario2-2-8core.txt
+echo "2"
+./test -cores 8 -height 2000 -iteration 6000 > result-scenario2-3-8core.txt
+echo "3"
+./test -cores 8 -height 2000 -iteration 6000 > result-scenario2-4-8core.txt
+echo "4"
+./test -cores 8 -height 2000 -iteration 6000 > result-scenario2-5-8core.txt
+echo "5"
+./test -cores 8 -height 2000 -iteration 6000 > result-scenario2-6-8core.txt
+echo "6"
+./test -cores 8 -height 2000 -iteration 6000 > result-scenario2-7-8core.txt
+echo "7"
+./test -cores 8 -height 2000 -iteration 6000 > result-scenario2-8-8core.txt
+echo "8"
